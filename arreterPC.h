cda8d387b95126999e4f2aa0a6d14048558a9192
g-gmu <46576219+g-gmu@users.noreply.github.com>
2019-06-09 21:34:37 +0200

    Commit initial

---
+++ arreterPC.h
@@ -0,0 +1,18 @@
+#ifndef ARRETERPC_H_INCLUDED
+#define ARRETERPC_H_INCLUDED
+
+class arreterPC
+    {
+    private:
+
+    public:
+        // Constructeur
+        arreterPC();
+        // Deconstructeur
+        ~arreterPC();
+        // Accesseurs et mutateurs
+        void arretDuPC();
+    };
+
+#endif // ARRETERPC_H_INCLUDED
+
---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include "arreterPC.h"
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    arreterPC A;
+    A.arretDuPC();
+
+    return 0;
+    }
